b179a8b1efc8a3ddb5441da46b7ecd4067388794
Aashni Shah <contact@aashni.me>
2016-02-20 05:13:33 -0500

    People show up in a list depending on their status

--- app.js
+++ app.js
@@ -33,7 +33,46 @@ weasleyClock.controller('HomeCtrl', [
 			name: 'Mark',
 			location: 'Home',
       image: 'http://vignette1.wikia.nocookie.net/villains/images/8/86/Draco_Malfoy_(Year_1).jpg/revision/latest?cb=20120806221046'
-		}
+		},
+    {
+      id: 0,
+      name: 'Jon',
+      location: 'Work',
+      image: 'https://upload.wikimedia.org/wikipedia/en/5/5e/Ron_Weasley_poster.jpg'
+    },
+    {
+      id: 1,
+      name: 'Mark',
+      location: 'Home',
+      image: 'http://vignette1.wikia.nocookie.net/villains/images/8/86/Draco_Malfoy_(Year_1).jpg/revision/latest?cb=20120806221046'
+    },
+    {
+      id: 0,
+      name: 'Jon',
+      location: 'Work',
+      image: 'https://upload.wikimedia.org/wikipedia/en/5/5e/Ron_Weasley_poster.jpg'
+    },
+    {
+      id: 1,
+      name: 'Mark',
+      location: 'Home',
+      image: 'http://vignette1.wikia.nocookie.net/villains/images/8/86/Draco_Malfoy_(Year_1).jpg/revision/latest?cb=20120806221046'
+    }
 	];
+
+    var home = [];
+    var work = [];
+    var peril = [];
+
+    angular.forEach(this.firebase, function(dude) {
+        if(dude.location == 'Home') {
+          home.push(dude);
+        } else if(dude.location == 'Work') {
+          work.push(dude);
+        } else {
+          peril.push(dude);
+        }
+    });
+    this.people = [home, work, peril];
   }
 ]);
